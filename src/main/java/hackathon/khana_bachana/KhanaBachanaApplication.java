b6dd83b077a9bbec414ce90457e3c965f216806e
rahuls-appdesk <rahul.s@appdeskservices.com>
2022-03-25 13:48:05 +0530

    first commit

---
+++ src/main/java/hackathon/khana_bachana/KhanaBachanaApplication.java
@@ -0,0 +1,13 @@
+package hackathon.khana_bachana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhanaBachanaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KhanaBachanaApplication.class, args);
+  }
+
+}
---
+++ src/test/java/hackathon/khana_bachana/KhanaBachanaApplicationTests.java
@@ -0,0 +1,13 @@
+package hackathon.khana_bachana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhanaBachanaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
